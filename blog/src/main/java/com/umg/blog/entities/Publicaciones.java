26203425f7b9493d3acf82f6b874dd441795c73b
joga17000 <joga17000@gmail.com>
2025-07-24 22:23:09 -0600

    Commit con nuevos cambios en clases de usuarios, publicaciones y comentarios.

--- blog/src/main/java/com/umg/blog/entities/Publicaciones.java
+++ blog/src/main/java/com/umg/blog/entities/Publicaciones.java
@@ -2,22 +2,25 @@ package com.umg.blog.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class Usuarios {
+public class Publicaciones {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombreUsuario;
-    private String correo;
+    private String titulo;
+    private String contenido;
+    private LocalDate fechaPublicacion;
 
-    @OneToMany(mappedBy = "usuario")
-    private List<Publicaciones> publicaciones;
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuarios usuario;
 
-    @OneToMany(mappedBy = "usuario")
+    @OneToMany(mappedBy = "publicacion")
     private List<Comentarios> comentarios;
 }
--- blog/src/main/java/com/umg/blog/entities/comentarios.java
+++ blog/src/main/java/com/umg/blog/entities/comentarios.java
@@ -1,5 +1,25 @@
 package com.umg.blog.entities;
 
-public class comentarios {
-    
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class Comentarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String contenido;
+    private LocalDate fechaComentario;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuarios usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "publicacion_id")
+    private Publicaciones publicacion;
 }
